862c2e9ccaa9a15d7c045e7642218206a18049a4
Andrey Golik <1858191+andreygolik@users.noreply.github.com>
2020-06-26 16:14:23 -0400

    course finished

---
+++ src/Modal/Modal.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import './Modal.css';
+
+export default class Modal extends React.Component {
+  state = {
+    isOpen: false,
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <button onClick={() => this.setState({ isOpen: true })} style={{ float: 'right', marginTop: '.5rem' }}>
+          Open Modal
+        </button>
+
+        {this.state.isOpen && (
+          <div className="modal">
+            <div className="modal-body">
+              <h1>Modal Title</h1>
+              <p>Modal body.</p>
+              <button onClick={() => this.setState({ isOpen: false })}>Close</button>
+            </div>
+          </div>
+        )}
+      </React.Fragment>
+    );
+  }
+}
